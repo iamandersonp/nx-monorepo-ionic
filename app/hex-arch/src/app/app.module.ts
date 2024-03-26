2144687d50e6f3e86e66fc33277b00a9d13cecce
Anderson Peñaloza <designeromega@gmail.com>
2024-03-26 18:33:40 +0100

    feat(app): :sparkles: add AppConfig implementation
    Signed-off-by: Anderson Peñaloza <designeromega@gmail.com>

--- app/hex-arch/src/app/app.module.ts
+++ app/hex-arch/src/app/app.module.ts
@@ -7,10 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './ui/app.component';
 
+import { APP_CONFIG } from '@iamanderson/app-config';
+import { environment } from '../enviroments/environment';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: APP_CONFIG, useValue: environment }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ app/hex-arch/src/enviroments/environment.prod.ts
@@ -0,0 +1,26 @@
+/**
+ * @ignore
+ */
+export const baseUrl = 'https://wbetest.bde.es/vit_web/vit';
+
+/**
+ * Used to set enviroments reusable elements
+ *
+ * @ignore
+ * @type {object}
+ */
+export const environment = {
+  production: true,
+  timeOut: 10000,
+  minInterval: 30000,
+  showLogs: false,
+  defaultLanguage: 'en',
+  languages: ['en'],
+  dbVersion: 1.0,
+  logLevel: 0,
+  baseUrl: 'https://api.tvmaze.com/',
+  storeKeys: {
+    DB_VERSION: 'db_version',
+    USER_LANGUAGE: 'user_language'
+  }
+};
